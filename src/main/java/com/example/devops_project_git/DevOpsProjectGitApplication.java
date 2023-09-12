51892b824d4088448607678ef9f750d0b42c9e7f
Ida Grufman Johansson <johansson.c.ida@gmail.com>
2023-09-12 13:45:55 +0200

    Commit1

---
+++ src/main/java/com/example/devops_project_git/DevOpsProjectGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.devops_project_git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsProjectGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevOpsProjectGitApplication.class, args);
+    }
+
+}
